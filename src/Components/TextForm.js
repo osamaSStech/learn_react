a21d0a4c01f1b017a192d1a65dde1f40d837313e
osamaSStech <osama.sstech@gmail.com>
2023-01-24 11:51:58 +0530

    vid-8, adding more logic to textUtils

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 export default function TextForm(props) {
-    const [text, setText] = useState('Please Enter the Text')
+    const [text, setText] = useState('')
     // text = "new text"; wrong way to change value of const text
     // setText("new text"); correct way to change value of const text // but it's not working
 
@@ -12,20 +12,32 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLoClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
     return (
         <>
-            {/* <div className="container my-3"> */}
             <div className="container my-3">
-                <form>
-                    <h1>{props.heading}</h1>
-                    <div className="form-group">
-                        <textarea className="form-control" id="myText" rows="8" value={text} onChange={handleOnChange}></textarea>
-                        {/* <textarea className="form-control" id="myText" rows="8" placeholder={text}></textarea> */}
-                    </div>
-                </form>
-                <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+                <div className="container my-3">
+                    <form>
+                        <h1>{props.heading}</h1>
+                        <div className="form-group">
+                            <textarea className="form-control" id="myText" rows="6" value={text} onChange={handleOnChange}></textarea>
+                            {/* <textarea className="form-control" id="myText" rows="8" placeholder={text}></textarea> */}
+                        </div>
+                    </form>
+                    <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+                    <button className="btn btn-primary float-right" onClick={handleLoClick}>Convert to Lowercase</button>
+                </div>
+                <div className="container my-3">
+                    <h2>Your text summary</h2>
+                    <p>{text.split(" ").length} words and {text.length} characters.</p>
+                    <p>{.008 * text.split(" ").length} minutes read.</p>
+                    <h2>Preview</h2>
+                    <p>{text}</p>
+                </div>
             </div>
-            {/* </div> */}
         </>
     )
 }
