9357ae1c8825d040305bdb48dcf378c6b2bbba67
osamaSStech <osama.sstech@gmail.com>
2023-01-24 17:56:20 +0530

    vid-10, Creating "Enable Dark Mode" Button Using useState Hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 // import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Navbar from './Components/Navbar.js';
-import TextForm from './Components/TextForm.js';
+// import TextForm from './Components/TextForm.js';
 // import Login from './Components/login.js';
 import './App.css';
+import AboutUs from './Components/AboutUs.js';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <Navbar /> */}
       {/* <Navbar title="Dynamic Title" aboutText="About" /> */}
       <Navbar title="Dynamic Title" />
-      <TextForm heading="Enter the text to analyze" />
+      {/* <TextForm heading="Enter the text to analyze" /> */}
+      <AboutUs />
       {/* <Login /> */}
     </>
   );
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -35,8 +35,8 @@ export default function TextForm(props) {
                 </div>
                 <div className="container my-3">
                     <h2>Your text summary</h2>
-                    <p>{((textStart == '') ? text.split(" ").length - 1 : text.split(" ").length)} words and {text.length} characters.</p>
-                    <p>{((textStart == '') ? 0 : 0.008 * text.split(" ").length)} minutes read.</p>
+                    <p>{((textStart === '') ? text.split(" ").length - 1 : text.split(" ").length)} words and {text.length} characters.</p>
+                    <p>{((textStart === '') ? 0 : 0.008 * text.split(" ").length)} minutes read.</p>
                     <h2>Preview</h2>
                     <p>{text}</p>
                 </div>
